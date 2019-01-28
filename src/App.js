03e5a8bb1c793bda112e39b82f6a0c9ab5a33935
rizkisadewa <rizkykysadewa@gmail.com>
2019-01-28 12:02:52 +0800

    create state in App.js, pass down to Todos components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import Todos from './components/Todos'
+import Todos from './components/Todos'; // import Todos component from folder components
+
+import './App.css'; // Import the CSS from App.css
 
 class App extends Component {
+
+  // State is like a database to create a value, each component will have a state.
+  // We can pass down the state into Todos as a props
+  state = {
+    todos : [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
   render() {
+    // console.log(this.state.todos) // will be shown the state in the console
     return (
-      // <Todos /> is the customized Tag that created in Todos.js
+      // <Todos /> is the customized Tag that created in Todos.js at components folder
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,16 @@
+// Uper Case each word is for the convention of the component
+
 import React, { Component } from 'react';
 
 // Make a new component that copy from App.js, but we just modified the class name and export default into Todos.
 // We do not need the tag with the calss App as that only in App.js.
 class Todos extends Component {
   render() {
+    // console.log(this.props.todos); // will be printed all of todos state that created in the state come from App.js
     return (
-      <h1>Todos</h1>
+      <div>
+        <h1>Todos</h1>
+      </div>
     );
   }
 }
